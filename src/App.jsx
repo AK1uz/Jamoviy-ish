9e001cb513654df0c44252ece0139f7ba5684eaf
AK1uz <kamolashukurova0991@gmail.com>
2026-03-03 10:29:02 +0300

    Yana o'zgradi

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Header from './components/Header'; 
+import Header from './components/Header';
 import SectionOne from './components/SectionOne';
 import SectionTwo from './components/SectionTwo';
+import Section1 from './components/Section1';
+import Section3 from './components/Section3';
+import Section2 from './components/Section2';
+import Section4 from './components/Section4';
 
 const App = () => {
   return (
     <>
       <Header />
       <SectionOne />
-      <SectionTwo/>
+      <SectionTwo />
+      <Section1 />
+      <Section3 />
+      <Section2 />
+      <Section4 />
     </>
   );
 }
